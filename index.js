d581f081ebab983cb02d3a16e07bd418de6b3e16
Nick Joyce <brawlyrox@gmail.com>
2020-05-15 18:46:13 +0100

    slight tweak to index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express')
 const app = express()
 
 app.get('/', (req, res) => {
-    res.send({ hi: 'there'})
+    res.send({ hello: 'world'})
 })
 
 const PORT = process.env.PORT || 5000
