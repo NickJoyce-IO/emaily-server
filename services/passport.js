5a0cffaba9c311ce438adf932072f520db8d9c45
Nick Joyce <brawlyrox@gmail.com>
2020-05-31 10:15:18 +0100

    Update Google Strategy to work with heroku proxy

--- services/passport.js
+++ services/passport.js
@@ -26,6 +26,7 @@ passport.use(
       clientID: keys.googleClientID,
       clientSecret: keys.googleClientSecret,
       callbackURL: '/auth/google/callback',
+      proxy: true
     },
     // gets called once we have been successfully back to the server and gives us
     // tokens back from Google
