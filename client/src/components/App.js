e877b746710aeacefd5dac6706690c101c2e2d5e
Nick Joyce <brawlyrox@gmail.com>
2020-06-05 19:39:15 +0100

    Set up the header and test the auth dispatch

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,6 +1,7 @@
-import React from 'react';
+import React, { Component } from 'react'
 import { BrowserRouter, Route } from 'react-router-dom'
-
+import { connect } from 'react-redux'
+import * as actions from '../actions'
 
 import Header from './Header'
 
@@ -8,9 +9,14 @@ const Dashboard = () => <h2>Dashboard</h2>
 const SurveyNew = () => <h2>SurveyNew</h2>
 const Landing = () => <h2>Landing</h2>
 
-const App = () => {
+class App extends Component {
+  componentDidMount() {
+     this.props.fetchUser()
+  }
+
+  render() {
     return (
-      <div>
+      <div className="container">
         <BrowserRouter>
           <div>
             <Header />
@@ -21,6 +27,7 @@ const App = () => {
         </BrowserRouter>
       </div>
     )
+  }
 }
 
-export default App;
+export default connect(null, actions)(App)
