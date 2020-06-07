600c1bcdfff47c00480f504f6c7d001712be7d33
Nick Joyce <brawlyrox@gmail.com>
2020-06-07 17:25:53 +0100

    add logic for serving production assets

--- index.js
+++ index.js
@@ -40,6 +40,20 @@ app.use(passport.session())
 require('./routes/authRoutes')(app)
 require('./routes/billingRoutes')(app)
 
+// code to make app behave when deployed to Prod/Heroku
+
+if (process.env.NODE_ENV === 'production') {
+  // Make sure express will server up prod assets, main.js or main.css
+  app.use(express.static('client/build'))
+
+  // Express will serve up index.html if it doesn't recognise the route
+  // CATCH ALL ROUTES, goes to index.html
+  const path = require('path')
+  app.get('*', (req,res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
+  })
+}
+
 const PORT = process.env.PORT || 5000
 
 app.listen(PORT)
