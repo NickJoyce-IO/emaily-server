25c6c3e8c9f9fe1b397bc03e15b30253449fe8e4
Nick Joyce <brawlyrox@gmail.com>
2020-08-31 15:53:04 +0100

    fix security dependencies in packages

--- routes/surveyRoutes.js
+++ routes/surveyRoutes.js
@@ -1,6 +1,8 @@
 const mongoose = require('mongoose')
 const requireLogin = require('../middlewares/requireLogin')
 const requireCredits = require('../middlewares/requireCredits')
+const Mailer = require('../services/mailer')
+const surveyTemplate = require('../services/emailTemplates/surveyTemplate')
 
 const Survey = mongoose.model('surveys')
 
@@ -17,6 +19,9 @@ module.exports = app => {
               dateSent: Date.now()
           })
 
+          // Send the email
+          const mailer = new Mailer(survey, surveyTemplate(survey))
+          
 
     })
 }
