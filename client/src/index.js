515bcaf57da60cb590ca0d70fde44787aa886351
Nick Joyce <brawlyrox@gmail.com>
2020-08-31 17:39:04 +0100

    Sort out the email template and tidy up route handler for email

--- client/src/index.js
+++ client/src/index.js
@@ -10,6 +10,11 @@ import reduxThunk from 'redux-thunk'
 import App from './components/App'
 import reducers from './reducers'
 
+// development only axios helpers
+import axios from 'axios'
+window.axios = axios
+
+
 
 const store = createStore (reducers, {}, applyMiddleware(reduxThunk))
 
